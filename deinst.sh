242a5047c6ac127a88dfd2a882e1bec923222afc
aquila62 <aquila62@users.noreply.github.com>
2016-06-12 11:53:52 -0500

    Add files via upload

---
+++ deinst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make -f eko.mak clean
+make -f hx.mak clean
+make -f ascii.mak clean
+make -f rlca.mak clean
+make -f rrca.mak clean
+make -f randu.mak clean
+make -f z80rnd.mak clean
---
+++ inst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make -f eko.mak
+make -f hx.mak
+make -f ascii.mak
+make -f rlca.mak
+make -f rrca.mak
+make -f randu.mak
+make -f z80rnd.mak
