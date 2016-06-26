ee02b3d502ac543ba9b80120f9341499bbe2fe4f
aquila62 <aquila62@users.noreply.github.com>
2016-06-26 11:05:31 -0500

    Added GPL text at the beginning

--- typ.asm
+++ typ.asm
@@ -1,10 +1,35 @@
-; This program was inspired by a Digital Research sample program
+; typ.asm - Print a text file to the console   Version 1.0.0
+; Copyright (C) 2016 aquila62 at github.com
+
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License as
+; published by the Free Software Foundation; either version 2 of
+; the License, or (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to:
+
+   ; Free Software Foundation, Inc.
+   ; 59 Temple Place - Suite 330
+   ; Boston, MA 02111-1307, USA.
+
+; This program was inspired by the Digital Research sample
+; file copy program
 ;
 ; text file browser program
 ;
 ; at the ccp level, the command usage:
 ;
 ; typ a:x.y
+;
+; Example:
+;
+; typ typ.asm
 ;
 kcin:   equ 0006h     ; keyboard input routine
 kcout:  equ 0009h     ; console output routine
